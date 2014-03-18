45fbbc81bd2b113684fb517832609404f2be48d4
Jose Garcia <jose.garcia12@upr.edu>
2014-03-17 21:34:37 -0400

    Minimum Files

---
+++ Project1/src/com/example/project1/SortedList.java
@@ -0,0 +1,27 @@
+package com.example.project1;
+
+
+
+import java.util.Iterator;
+
+public interface SortedList<E extends Comparable<E>> extends Iterable<E>{
+	// valid position in the list
+	// [0, size() - 1]
+	
+	public boolean add(E obj);
+	public int size();
+	public boolean remove(E obj);
+	public boolean remove(int index);
+	public int removeAll(E obj);
+	public E first();
+	public E last();
+	public E get(int index);
+	public void clear();
+	public boolean contains(E e);
+	public boolean isEmpty();
+	public Iterator<E> iterator(int index);
+	public int firstIndex(E e);
+	public int lastIndex(E e);
+
+	
+}
